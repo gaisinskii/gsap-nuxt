63f62d3f78be6c1b0afa715ae11839542b5b5e87
andrey gaisinskii <andygaisinskii@gmail.com>
2018-07-12 17:29:09 +0300

    initial commit

---
+++ components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>Test</h1>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main-component></main-component>
+</template>
+
+<script>
+import MainComponent from '~/components/MainComponent.vue';
+
+export default {
+  components: {
+    MainComponent,
+  }
+}
+</script>
+
+<style>
+</style>
